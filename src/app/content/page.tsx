60aa71251e7eedecc116fb42d86ecad499d51ca5
lingjue <3266764066@qq.com>
2024-07-19 01:30:47 +0800

    bug(router)

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -2,12 +2,15 @@
 import React from "react";
 import Content from "@/components/content/Content";
 import Navbar from "@/components/content/Navbar";
+import { Suspense } from "react";
 
 export default function page() {
   return (
     <div className="flex w-full h-full bg-white overflow-hidden">
       <div className="w-3/4 min-h-screen">
-        <Content />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Content />
+        </Suspense>
       </div>
       <div className="w-1/4 min-h-screen">
         <Navbar />
--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -5,12 +5,15 @@ import React from "react";
 import Layout from "../layout";
 // 导入组件
 import RoadmapComp from "../../components/roadmap/RoadmapComp";
+import { Suspense } from "react";
 
 export default function page() {
   return (
     <Layout>
       <div className="h-screen w-screen">
-        <RoadmapComp />
+        <Suspense fallback={<div>Loading...</div>}>
+          <RoadmapComp />
+        </Suspense>
       </div>
     </Layout>
   );
